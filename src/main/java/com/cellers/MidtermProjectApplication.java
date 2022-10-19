8bfc7886fcfd56c53f1a3e6fb7b6c5bb46c6d69a
Jayden Cellers <jayden.cellers@hcl.com>
2022-10-19 03:08:33 -0500

    Initial commit

---
+++ src/main/java/com/cellers/MidtermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cellers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cellers/user/UserEntity.java
@@ -0,0 +1,39 @@
+package com.cellers.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+}
